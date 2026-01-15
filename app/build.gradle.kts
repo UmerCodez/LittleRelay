79918731b4705e3581f2ccc9f67db7b820d2a8bd
UmerCodez <umerfarooq.phone@gmail.com>
2026-01-15 21:59:33 +0500

    build: Bump version to 1.0.0-beta.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "app.umerfarooq.littlerelay"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0-beta.2"
+        versionCode = 3
+        versionName = "1.0.0-beta.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
