5a436876b8352caf28a16c807bff4ed3bfe8d11e
UmerCodez <umerfarooq.phone@gmail.com>
2026-02-04 18:42:19 +0500

    build: Bump app version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "app.umerfarooq.littlerelay"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
